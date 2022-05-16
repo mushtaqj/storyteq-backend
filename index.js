8159641e2e82f0252878f69bde4b4602a6b45323
Mushtaq Jameel <mushtaq.jameel@villvay.com>
2022-05-16 14:52:52 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// Sample 2
+const initialList = [2, 4, 6, 8, 10]; // Here is our initial oredered list
+const shiftedList = [6, 8, 10, 2, 4]; // This is our shifted list
+
+/**
+ *
+ * @param {[]} numArray
+ * @param {[]} initList optional, passed the default initialList if none given.
+ * Not sure if this is can be used ,but since is there used to optimize the sequences
+ * @returns
+ */
+function findLargestIntIn(numArray, initList = initialList) {
+  let [max] = numArray;
+  const [firstElInInitList] = initList;
+  let iterationCount = 0;
+
+  if (max == firstElInInitList) {
+    max = numArray[numArray.length - 1];
+  } else {
+
+    for (let i = 1; i < numArray.length; i++) {
+      const currEl = numArray[i];
+      iterationCount++;
+
+      const isGreaterThanMax = currEl - max >= 0;
+
+      if (isGreaterThanMax) {
+        max = currEl;
+      } else {
+        break;
+      }
+    }
+  }
+  return { max, iterationCount };
+}
+
+console.log(findLargestIntIn(shiftedList));
