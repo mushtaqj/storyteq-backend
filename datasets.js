f293841dcc62bec060091a8462cdb476f794de5e
Mushtaq Jameel <mushtaq.jameel@villvay.com>
2022-05-16 18:22:34 +0530

    feat : add datasets to make the indexjs more readable

---
+++ datasets.js
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+const normalCaseDataSet = {
+  initialList: [
+    1, 3, 7, 8, 9, 10, 11, 13, 15, 17, 19, 21, 22, 25, 55, 57, 100, 102, 103,
+    104, 105, 106, 107, 108, 200, 202, 220, 230, 330,
+  ],
+  shiftedList: [
+    100, 102, 103, 104, 105, 106, 107, 108, 200, 202, 220, 230, 330, 1, 3, 7, 8,
+    9, 10, 11, 13, 15, 17, 19, 21, 22, 25, 55, 57,
+  ],
+};
+
+/**
+ * Array is already sorted in descending order, so this would be a constant O(n)
+ */
+const bestCaseDataSet = {
+  initialList: [
+    1, 3, 7, 8, 9, 10, 11, 13, 15, 17, 19, 21, 22, 25, 55, 57, 100, 102, 103,
+    104, 105, 106, 107, 108, 200, 202, 220, 230, 330,
+  ],
+  shiftedList: [
+    330, 230, 220, 202, 200, 107, 108, 106, 105, 104, 102, 103, 100, 57, 55, 25,
+    22, 21, 19, 17, 15, 13, 11, 10, 9, 8, 7, 3, 1,
+  ],
+};
+
+/**
+ * Array is already sorted in ascending order, Can be the best case alorithm if we use the initialList and worst performing if the list cannot be used for context O(1) or O(n)
+ */
+const worseCaseDataSet = {
+  initialList: [
+    1, 3, 7, 8, 9, 10, 11, 13, 15, 17, 19, 21, 22, 25, 55, 57, 100, 102, 103,
+    104, 105, 106, 107, 108, 200, 202, 220, 230, 330,
+  ],
+  shiftedList: [
+    1, 3, 7, 8, 9, 10, 11, 13, 15, 17, 19, 21, 22, 25, 55, 57, 100, 102, 103,
+    104, 105, 106, 107, 108, 200, 202, 220, 230, 330,
+  ],
+};
+
+module.exports = {
+  normalCaseDataSet,
+  worseCaseDataSet,
+  bestCaseDataSet,
+};
